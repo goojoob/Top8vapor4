0fcf225b8096c1ae2772ecb317a149b34f04df47
Antonio Rodríguez <antrorc@gmail.com>
2021-05-19 12:42:30 +0200

    CORS middleware

--- configure.swift
+++ configure.swift
@@ -3,6 +3,23 @@ import FluentSQLiteDriver
 import Vapor
 
 public func configure(_ app: Application) throws {
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+
+    // Only add this if you want to enable the default per-route logging
+    let routeLogging = RouteLoggingMiddleware(logLevel: .info)
+
+    // Add the default error middleware
+    let error = ErrorMiddleware.default(environment: app.environment)
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(routeLogging)
+    app.middleware.use(error)
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
